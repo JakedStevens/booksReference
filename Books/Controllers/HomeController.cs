c567250185ac8ee4c3c39fc9150b000c2e0de59e
Jake Stevens <jake.stevens@improving.com>
2021-05-24 16:59:25 -0500

    updates from past couple of days

--- Books/Controllers/HomeController.cs
+++ Books/Controllers/HomeController.cs
@@ -1,42 +1,51 @@
-﻿using System;
+﻿using Books.Models;
+using Books.ViewModels.Home;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Books.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace Books.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly BookData _bookData;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration, BookData bookData)
         {
             _logger = logger;
+            _configuration = configuration;
+            _bookData = bookData;
         }
 
         public IActionResult Index()
         {
-            using (var con = new SqlConnection("Server=.;Database=Books;Trusted_Connection=True;"))
-            {
-                con.Open();
-                var command = new SqlCommand();
-                command.Connection = con;
-                command.CommandType = System.Data.CommandType.Text;
-                command.CommandText = "SELECT * FROM Books";
-                var reader = command.ExecuteReader();
+            var books = _bookData.AllBookData();
 
-                while (reader.Read())
-                {
-                    var title = reader["Title"].ToString();
-                    var id = Convert.ToInt32(reader["BookId"]);
-                }
-            }
+            var vm = new HomeViewModel();
+            vm.Message = "Look at these wonderful books!";
+            vm.Books = books;
+
+            return View(vm);
+        }
+
+        public IActionResult BooksWithAjax()
+        {
             return View();
+        }
+
+        public IActionResult BookData()
+        {
+            var books = _bookData.AllBookData();
+
+            return Json(books);
         }
 
         public IActionResult Privacy()
--- Books/Startup.cs
+++ Books/Startup.cs
@@ -1,13 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Books.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Books
 {
@@ -24,6 +25,8 @@ namespace Books
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddScoped<BookData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
